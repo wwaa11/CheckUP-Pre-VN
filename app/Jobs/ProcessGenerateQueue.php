adf036e33c3656fdfc43ccb16462ea24f6ae02ab
Pakawa Kaphondee <pakawa123@gmail.com>
2025-09-29 04:40:58 +0000

    Update Process

--- app/Jobs/ProcessGenerateQueue.php
+++ app/Jobs/ProcessGenerateQueue.php
@@ -1,35 +1,24 @@
 <?php
-
 namespace App\Jobs;
 
 use App\Models\Master;
 use App\Models\Number;
 use App\Models\Time;
+use Illuminate\Contracts\Queue\ShouldBeUniqueUntilProcessing;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Support\Facades\Log;
 use Throwable;
 
-class ProcessGenerateQueue implements ShouldQueue
+class ProcessGenerateQueue implements ShouldQueue, ShouldBeUniqueUntilProcessing
 {
     use Queueable;
-    public $tries = 50;
+    public $tries   = 50;
     public $backoff = 3;
-
-    public function middleware(): array
-    {
-        return [new WithoutOverlapping('Generate Number')];
-    }
 
     public function uniqueId(): string
     {
         return 'Generate Number';
-    }
-
-    public function __construct()
-    {
-
     }
 
     public function handle(): void
@@ -39,75 +28,40 @@ class ProcessGenerateQueue implements ShouldQueue
             ->orderBy('check_in', 'asc')
             ->get();
 
-        foreach($masters as $item){
+        foreach ($masters as $item) {
             $getNumber = Number::where('date', date('Y-m-d'))->first();
             if ($getNumber == null) {
-                $newDate = new Number;
+                $newDate       = new Number;
                 $newDate->date = date('Y-m-d');
                 $newDate->save();
                 $getNumber = Number::where('date', date('Y-m-d'))->first();
             }
-            $type = $item->type;
-            $number = $getNumber->$type + 1;
-            $queueNumber = $type . str_pad($number, 3, '0', STR_PAD_LEFT);
+            $type             = $item->type;
+            $number           = $getNumber->$type + 1;
+            $queueNumber      = $type . str_pad($number, 3, '0', STR_PAD_LEFT);
             $getNumber->$type = $number;
             $getNumber->save();
 
-            $arrayQueue = Time::where('station', 'checkup')->where('type', $type)->first();
+            $arrayQueue       = Time::where('station', 'checkup')->where('type', $type)->first();
             $arrayQueue->list = json_decode($arrayQueue->list);
-            $temp_list = $arrayQueue->list;
-            if (!in_array($queueNumber, $temp_list)) {
+            $temp_list        = $arrayQueue->list;
+            if (! in_array($queueNumber, $temp_list)) {
                 array_push($temp_list, $queueNumber);
                 $arrayQueue->list = json_encode($temp_list);
                 $arrayQueue->save();
-                Log::channel('daily')->notice($item->hn.' generate ' . $queueNumber . ' to ' . $type);
+                Log::channel('daily')->notice($item->hn . ' generate ' . $queueNumber . ' to ' . $type);
 
                 $item->number = $queueNumber;
                 $item->save();
             }
         }
-        
+
         ProcessGenerateQueue::dispatch()->delay(1);
     }
 
     public function failed(?Throwable $exception): void
     {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://api.line.me/v2/bot/message/push',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS =>'{
-            "to": "U3d7ba4f0386437906a68612c1cce5eba",
-            "messages":[
-                {
-                    "type":"text",
-                    "text":"Services Error",
-                    "quickReply": {
-                        "items": [
-                        {
-                            "type": "action",
-                            "action": {
-                                "type": "uri",
-                                "label": "Generate Number Error",
-                                "uri": "https://pr9webhub.praram9.com/checkup/serviceStart"
-                            }
-                        }
-                        ]
-                    }
-                }
-            ]
-        }',
-        CURLOPT_HTTPHEADER => array(
-            'Authorization: Bearer '.env('LINE_Token').'','Content-Type: application/json'
-        ),
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
+        Log::channel('services')->error('ProcessGenerateQueue failed: ' . $exception->getMessage());
+        ProcessGenerateQueue::dispatch()->delay(10);
     }
 }
--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default'      => env('LOG_CHANNEL', 'stack'),
 
     /*
     |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ return [
 
     'deprecations' => [
         'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
-        'trace' => env('LOG_DEPRECATIONS_TRACE', false),
+        'trace'   => env('LOG_DEPRECATIONS_TRACE', false),
     ],
 
     /*
@@ -50,102 +50,106 @@ return [
     |
     */
 
-    'channels' => [
+    'channels'     => [
 
-        'stack' => [
-            'driver' => 'stack',
-            'channels' => explode(',', env('LOG_STACK', 'single')),
+        'stack'      => [
+            'driver'            => 'stack',
+            'channels'          => explode(',', env('LOG_STACK', 'single')),
             'ignore_exceptions' => false,
         ],
 
-        'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+        'single'     => [
+            'driver'               => 'single',
+            'path'                 => storage_path('logs/laravel.log'),
+            'level'                => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
         ],
 
-        'line' => [
+        'line'       => [
             'driver' => 'single',
-            'path' => storage_path('logs/LineAPI.log'),
-            'level' => 'debug',
+            'path'   => storage_path('logs/LineAPI.log'),
+            'level'  => 'debug',
         ],
 
-        'insert' => [
+        'services'   => [
             'driver' => 'single',
-            'path' => storage_path('logs/Insert.log'),
-            'level' => 'debug',
+            'path'   => storage_path('logs/Services.log'),
+            'level'  => 'debug',
         ],
 
-        'debug' => [
+        'insert'     => [
             'driver' => 'single',
-            'path' => storage_path('logs/Debug.log'),
-            'level' => 'debug',
+            'path'   => storage_path('logs/Insert.log'),
+            'level'  => 'debug',
         ],
-        
-        'daily' => [
-            'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
-            'days' => env('LOG_DAILY_DAYS', 14),
+
+        'debug'      => [
+            'driver' => 'single',
+            'path'   => storage_path('logs/Debug.log'),
+            'level'  => 'debug',
+        ],
+
+        'daily'      => [
+            'driver'               => 'daily',
+            'path'                 => storage_path('logs/laravel.log'),
+            'level'                => env('LOG_LEVEL', 'debug'),
+            'days'                 => env('LOG_DAILY_DAYS', 14),
             'replace_placeholders' => true,
         ],
 
-        'slack' => [
-            'driver' => 'slack',
-            'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => env('LOG_SLACK_USERNAME', 'Laravel Log'),
-            'emoji' => env('LOG_SLACK_EMOJI', ':boom:'),
-            'level' => env('LOG_LEVEL', 'critical'),
+        'slack'      => [
+            'driver'               => 'slack',
+            'url'                  => env('LOG_SLACK_WEBHOOK_URL'),
+            'username'             => env('LOG_SLACK_USERNAME', 'Laravel Log'),
+            'emoji'                => env('LOG_SLACK_EMOJI', ':boom:'),
+            'level'                => env('LOG_LEVEL', 'critical'),
             'replace_placeholders' => true,
         ],
 
         'papertrail' => [
-            'driver' => 'monolog',
-            'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
+            'driver'       => 'monolog',
+            'level'        => env('LOG_LEVEL', 'debug'),
+            'handler'      => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
             'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'host'             => env('PAPERTRAIL_URL'),
+                'port'             => env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
-            'processors' => [PsrLogMessageProcessor::class],
+            'processors'   => [PsrLogMessageProcessor::class],
         ],
 
-        'stderr' => [
-            'driver' => 'monolog',
-            'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => StreamHandler::class,
-            'formatter' => env('LOG_STDERR_FORMATTER'),
-            'with' => [
+        'stderr'     => [
+            'driver'     => 'monolog',
+            'level'      => env('LOG_LEVEL', 'debug'),
+            'handler'    => StreamHandler::class,
+            'formatter'  => env('LOG_STDERR_FORMATTER'),
+            'with'       => [
                 'stream' => 'php://stderr',
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
 
-        'syslog' => [
-            'driver' => 'syslog',
-            'level' => env('LOG_LEVEL', 'debug'),
-            'facility' => env('LOG_SYSLOG_FACILITY', LOG_USER),
+        'syslog'     => [
+            'driver'               => 'syslog',
+            'level'                => env('LOG_LEVEL', 'debug'),
+            'facility'             => env('LOG_SYSLOG_FACILITY', LOG_USER),
             'replace_placeholders' => true,
         ],
 
-        'errorlog' => [
-            'driver' => 'errorlog',
-            'level' => env('LOG_LEVEL', 'debug'),
+        'errorlog'   => [
+            'driver'               => 'errorlog',
+            'level'                => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
         ],
 
-        'null' => [
-            'driver' => 'monolog',
+        'null'       => [
+            'driver'  => 'monolog',
             'handler' => NullHandler::class,
         ],
 
-        'emergency' => [
+        'emergency'  => [
             'path' => storage_path('logs/laravel.log'),
         ],
-
-
 
     ],
 
